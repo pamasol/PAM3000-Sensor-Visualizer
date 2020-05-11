c830a02e1c2bf64b0d6b4471e933a01f4ca5de33
david-bernhard <david.bernhard@pamasol.com>
2020-05-11 17:10:06 +0200

    Analoganzeige

--- PAM3000-Sensor-Visualizer.ino
+++ PAM3000-Sensor-Visualizer.ino
@@ -40,7 +40,14 @@
 #include <Diablo_Const4D.h>
 #include <Diablo_Serial_4DLib.h>
 
+#include <Wire.h>
+#include <Adafruit_INA219.h> // You will need to download this library
+
+Adafruit_INA219 ina219; // Declare and instance of INA219
+
 Diablo_Serial_4DLib Display(&DisplaySerial);  //Serielle Kommunikation mit Display
+
+
 
 //****************************************************************************************************************************************
 
@@ -216,6 +223,9 @@ void setup()
   Stopp_Zeit_Geschwindigkeit4 = 0;
 
   pinMode(HALL_SENSOR, INPUT);
+
+  Serial.begin(9600);    
+  sensor219.begin();
   
 }
 
@@ -900,6 +910,9 @@ if (Geschwindigkeit_Stopp2 == true)
       
 
       break;
+
+
+
 
 
   }
@@ -1599,8 +1612,6 @@ void Geschwindigkeitsanzeige_Durchschnitt()
     Display.gfx_Button(Status_Taste, 350, 400, YELLOW, BLACK, Schrifttyp, Textbreite, Textrahmen, "Reset"); //Taste anzeigen ungedrückt
     Display.gfx_Rectangle(10, 70, 790, 200, AQUA); //Rahmen zeichnen  (Rechteck x1,y1,x2,y2)
     Display.gfx_CircleFilled(40, 120, 20, RED); //Status Anzeigen roter Punkt (Kreis x,y,r)
-    Display.gfx_Rectangle(10, 330, 790, 200, AQUA); //Rahmen zeichnen  (Rechteck x1,y1,x2,y2)
-    Display.gfx_CircleFilled(40, 250, 20, RED); //Status Anzeigen roter Punkt (Kreis x,y,r)
 
     Bild_aufgebaut[3] = true;                   //Bildschirm aufgebaut setzen
 
@@ -1766,9 +1777,138 @@ void Analogeanzeige()
 
 {
 
+  Displaystopp = true;
+
+  if (Bild_aufgebaut[6] == false) //Überwachung Bildschirm aufgebaut
+
+   {
+
+    Display.gfx_Cls();                //Display löschen
+
+    Display.gfx_Button(Status_Taste, 40, 10, Texthintergrund, Textfarbe, Schrifttyp, Textbreite, Textrahmen, "Geschwindigkeitsanzeige Durchschnitt");
+
+    Status_Taste = BUTTON_UP;
+
+    Display.gfx_Button(Status_Taste, 650, 400, Texthintergrund, Textfarbe, Schrifttyp, Textbreite, Textrahmen, "Zrug"); //Taste anzeigen ungedrückt
+    Display.gfx_Button(Status_Taste, 350, 400, YELLOW, BLACK, Schrifttyp, Textbreite, Textrahmen, "Reset"); //Taste anzeigen ungedrückt
+    Display.gfx_Rectangle(10, 70, 790, 200, AQUA); //Rahmen zeichnen  (Rechteck x1,y1,x2,y2)
+    Display.gfx_CircleFilled(40, 120, 20, RED); //Status Anzeigen roter Punkt (Kreis x,y,r)
+
+    Bild_aufgebaut[6] = true;                   //Bildschirm aufgebaut setzen
+
+  }
 
 
+  float busVoltage = 0;
+  float current = 0; // Measure in milli amps
+  float power = 0;
+
+  busVoltage = sensor219.getBusVoltage_V();
+  current = sensor219.getCurrent_mA();
+  power = busVoltage * (current/1000); // Calculate the Power
+  
+  
+  Serial.print("Bus Voltage:   "); 
+  Serial.print(busVoltage); 
+  Serial.println(" V");  
+  
+  Serial.print("Current:       "); 
+  Serial.print(current); 
+  Serial.println(" mA");
+  
+  Serial.print("Power:         "); 
+  Serial.print(power); 
+  Serial.println(" W");  
+  
+  Serial.println("");  
+  
+   Display.txt_Height(2);                        //Texthöhe
+   Display.txt_Width(2);                         //Textweite
+   Display.gfx_MoveTo(580, 100);                 //Text Position x,y
+   Display.print(busVoltage);          //Wert Anzeigen
+   Display.print(" V");
+   Display.gfx_MoveTo(580, 125);                 //Text Position x,y
+   Display.print(current);          //Wert Anzeigen
+   Display.print(" mA");
+   Display.gfx_MoveTo(580, 150);                 //Text Position x,y
+   Display.print(power);          //Wert Anzeigen
+   Display.print(" W");
+
+       
+   delay(2000);
+   
+
+    Status_Display = Display.touch_Get(TOUCH_STATUS);    //Status Touch Screen
+
+
+
+  if ((Status_Display == TOUCH_PRESSED) || (Status_Display == TOUCH_MOVING)) //Überwachung Touch gedrückt oder bewegt
+
+  {
+
+    X_Pos = Display.touch_Get(TOUCH_GETX);    //X Position auslesen
+    Y_Pos = Display.touch_Get(TOUCH_GETY);    //Y Position auslesen
+
+  }
+
+
+  //Touch Schalter auswertung
+
+
+  if (Status_Display == TOUCH_RELEASED)                       //Überwachung Touch Freigabe
+
+  {
+
+    
+    //Touch Feld Zurück
+
+
+    if ((X_Pos >= 620) && (X_Pos <= 790) && (Y_Pos >= 370) && (Y_Pos <= 430)) //Überwachung Touch Feld
+
+    {
+
+      Status_Taste = !Status_Taste;
+
+      Display.gfx_Button(Status_Taste, 650, 400, Texthintergrund_2, Textfarbe_2, Schrifttyp, Textbreite, Textrahmen, "Zrug"); //Taste anzeigen gedrückt
+
+
+      //Menü Hauptmenü anwählen
+
+
+      if (Status_Taste)
+
+      {
+
+        Menue_angewaehlt = 0;        //Menü anwählen
+
+        Bild_aufgebaut[6] = false;  //Bildschirm aufgebaut zurücksetzen
+
+      }
+
+    }
+ if ((X_Pos >= 320) && (X_Pos <= 500) && (Y_Pos >= 370) && (Y_Pos <= 430)) //Überwachung Touch Feld
+
+    {
+
+      Status_Taste = !Status_Taste;
+
+      Display.gfx_Button(Status_Taste, 350, 400, YELLOW, WHITE, Schrifttyp, Textbreite, Textrahmen, "RESET"); //Taste anzeigen gedrückt
+
+
+      if (Status_Taste)
+
+      {
+        
+        Menue_angewaehlt = 3;        //Menü anwählen
+
+        Bild_aufgebaut[6] = false;  //Bildschirm aufgebaut zurücksetzen
+
+      }
+
+    }
+  }  
 }
+
 
 
 void Magnetpolanzeige()
